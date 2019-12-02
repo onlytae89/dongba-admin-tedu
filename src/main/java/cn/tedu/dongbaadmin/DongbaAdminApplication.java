d6e4a2c6f79a20c56a1f9e17762ba30b42bd58de
208352363 <1820917458@qq.com>
2019-12-02 10:19:45 +0800

    初次Commit

---
+++ src/main/java/cn/tedu/dongbaadmin/DongbaAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.tedu.dongbaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongbaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DongbaAdminApplication.class, args);
+    }
+
+}

944077596e0c17cb4de6cbd93f941a98ea321f9c
208352363 <1820917458@qq.com>
2019-12-03 11:30:34 +0800

    补充PageObject内容, 初步完成SysLogService

--- src/main/java/cn/tedu/dongbaadmin/service/SysLogService.java
+++ src/main/java/cn/tedu/dongbaadmin/service/SysLogService.java
@@ -1,4 +1,10 @@
 package cn.tedu.dongbaadmin.service;
 
+import cn.tedu.dongbaadmin.common.vo.PageObject;
+import cn.tedu.dongbaadmin.entity.SysLog;
+
 public interface SysLogService {
+
+    PageObject<SysLog> findPageObjects(String username, Integer pageCurrent);
+
 }

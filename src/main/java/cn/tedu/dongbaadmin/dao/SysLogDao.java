37402f165b9671eb839e8d1ba2ca95e0ba8e96bf
208352363 <1820917458@qq.com>
2019-12-03 10:12:18 +0800

    初步完成SysLog的DAO层

--- src/main/java/cn/tedu/dongbaadmin/dao/SysLogDao.java
+++ src/main/java/cn/tedu/dongbaadmin/dao/SysLogDao.java
@@ -1,7 +1,30 @@
 package cn.tedu.dongbaadmin.dao;
 
-import org.springframework.stereotype.Repository;
+import cn.tedu.dongbaadmin.entity.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-@Repository
-public class SysLogDao {
+import java.util.List;
+
+@Mapper
+public interface SysLogDao {
+
+    /**
+     * 统计某个用户的日志总数
+     * @param username 用户名，支持模糊查询，可为null
+     * @return 该用户的日志数量
+     */
+    int getRowCount(@Param("username") String username);
+
+    /**
+     * 按页查询某个用户的日志
+     * @param username 用户名，支持模糊查询，可为null
+     * @param startIndex 页数，从1开始
+     * @param pageSize 每页记录数
+     * @return 当前页的日志列表
+     */
+    List<SysLog> findPageObjects(@Param("username") String username,
+                                 @Param("startIndex") Integer startIndex,
+                                 @Param("pageSize") Integer pageSize);
+
 }
